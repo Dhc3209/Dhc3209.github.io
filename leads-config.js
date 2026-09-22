57b3d7c0e7d4ea9fea0331154a8853ec1e27e8c3
Dhc3209 <cpr2643@gmail.com>
2026-09-21 20:13:23 -0400

    Route website leads via Google Apps Script

---
+++ leads-config.js
@@ -0,0 +1,3 @@
+/** CPR lead endpoint — Google Apps Script web app */
+window.CPR_LEADS_ENDPOINT =
+  "https://script.google.com/macros/s/AKfycbxVbxf9-4B_kxX_jqclFuCJqbk-FgDceFtGiQG-64tDUMJqnLbbgizInEM7vzuk-XBW3g/exec";
